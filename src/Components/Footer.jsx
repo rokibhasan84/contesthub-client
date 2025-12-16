fe9ee813b4b18392633b64a5807faf16b78b21a4
RokibHassan-PH <rokibhassan75@gmail.com>
2025-12-16 21:15:54 +0800

    added img tag from laptop

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <div className="bg-gray-900 text-white py-6 text-center">
-      <h2 className="font-bold text-xl">ContestHub</h2>
+      <div>
+          <img src="" alt="" className='w-10 rounded-full'/>
+          <h2 className="font-bold text-xl">ContestHub</h2>
+      </div>
+      
       <p>© 2025 ContestHub — All Rights Reserved</p>
       <div className="flex justify-center gap-4 mt-2">
         <a>Facebook</a>
